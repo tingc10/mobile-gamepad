3b194e8160d0aee6244e4eb9262fa8554326917f
Ting Chen <tingshen.chen@gmail.com>
2019-07-21 23:05:28 -0400

    Fix host

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,8 @@
 import io from 'socket.io-client';
 
 const isProd = process.env.NODE_ENV === 'production';
-const host = isProd ? 'https://young-gorge-52676.herokuapp.com' : 'http://localhost';
-export const socket = io(`${host}:52300`, {
+const host = isProd ? 'https://young-gorge-52676.herokuapp.com' : 'http://localhost:52300';
+export const socket = io(host, {
   query: {
     type: 'controller'
   }
