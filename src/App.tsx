c16f9d9c6e7399fb323fff9d773dcf16f72c0cdb
Ting Chen <tingshen.chen@gmail.com>
2019-12-11 00:54:27 -0500

    Stylize gamepad. Force landscape

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import NoSleep from 'nosleep.js';
-import Bowser from 'bowser';
-// import {socket} from './utils/socket';
-import './App.css';
-import { socket } from './utils/socket';
+import React, { useState, useEffect } from "react";
+import NoSleep from "nosleep.js";
+import Bowser from "bowser";
+import "./App.css";
+import { socket } from "./utils/socket";
 
 let keydownValue = 0;
 const parser = Bowser.getParser(window.navigator.userAgent);
+
+declare global {
+  interface Window {
+    DeviceMotionEvent: DeviceMotionEvent & {
+      requestPermission?: () => Promise<string>;
+    };
+  }
+}
+
+const RestartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
+      style={{ fill: "#fff" }}
+    >
+      <path d="M 10 0 L 0 2 L 3.03125 5.03125 C 1.273438 7.222656 0.1875 9.972656 0.1875 13 C 0.1875 20.074219 5.921875 25.8125 13 25.8125 C 20.078125 25.8125 25.8125 20.074219 25.8125 13 C 25.8125 7.695313 22.59375 3.132813 18 1.1875 L 18 4.28125 C 21.027344 6.019531 23.0625 9.261719 23.0625 13 C 23.0625 18.5625 18.5625 23.0625 13 23.0625 C 7.4375 23.0625 2.9375 18.5625 2.9375 13 C 2.9375 10.726563 3.695313 8.652344 4.96875 6.96875 L 8 10 Z"></path>
+    </svg>
+  );
+};
 
 const App: React.FC = () => {
   let isLandscape = true;
   const [, setOrientation] = useState();
   const [showRestartButton, setShowRestartButton] = useState(false);
-  const [status, setStatus] = useState('Please enter the clientId to pair controller');
+  const [status, setStatus] = useState(
+    "Please enter the clientId to pair controller"
+  );
   const [activeInputs, setActiveInputs] = useState<Set<string>>(new Set());
-  const [clientId, setClientId] = useState('');
-  if(window.innerHeight > window.innerWidth) {
-      isLandscape = false;
+  const [clientId, setClientId] = useState("");
+  const [
+    deviceOrientationInitialized,
+    setDeviceOrientationInitialized
+  ] = useState(false);
+  const [playerNumber, setPlayerNumber] = useState(1);
+  if (window.innerHeight > window.innerWidth) {
+    isLandscape = false;
   }
 
   function handleOrientation(event: DeviceOrientationEvent) {
-    let x = event.beta || 0;  // In degree in the range [-180,180]
+    let x = event.beta || 0; // In degree in the range [-180,180]
     let y = event.gamma || 0; // In degree in the range [-90,90]
+    if (playerNumber === 2) {
+      x = -x;
+      y = -y;
+    }
     setOrientation([x, y]);
-    socket.emit('deviceOrientationChanged', { x: ( x / 20 ), y: 0 });
+    socket.emit("deviceOrientationChanged", { x: x / 20, y: 0 });
   }
   useEffect(() => {
     const noSleep = new NoSleep();
-    window.addEventListener('deviceorientation', handleOrientation);
-    window.addEventListener('keyup', (e) => {
-      if (activeInputs.has('keydown')) {
-        activeInputs.delete('keydown');
-        socket.emit('deleteActiveInput', { inputType: 'keydown' });
+    window.addEventListener("keyup", e => {
+      if (activeInputs.has("keydown")) {
+        activeInputs.delete("keydown");
+        socket.emit("deleteActiveInput", { inputType: "keydown" });
         setActiveInputs(activeInputs);
       }
     });
 
-    window.addEventListener('keydown', (e) => {
-      if (!activeInputs.has('keydown')) {
-        activeInputs.add('keydown');
-        socket.emit('newActiveInput', { inputType: 'keydown' });
+    window.addEventListener("keydown", e => {
+      if (!activeInputs.has("keydown")) {
+        activeInputs.add("keydown");
+        socket.emit("newActiveInput", { inputType: "keydown" });
         setActiveInputs(activeInputs);
       }
 
@@ -50,54 +81,48 @@ const App: React.FC = () => {
         if (keydownValue < 50) {
           keydownValue++;
         }
-      } else if(e.keyCode === 37) {
+      } else if (e.keyCode === 37) {
         if (keydownValue > 0) {
           keydownValue = 0;
         }
         if (keydownValue > -50) {
           keydownValue--;
-
         }
       }
-      socket.emit('deviceOrientationChanged', { x: keydownValue, y: 0 });
+      socket.emit("deviceOrientationChanged", { x: keydownValue, y: 0 });
     });
 
-    socket.on('controllerPairError', (data: { message: string }) => {
+    socket.on("controllerPairError", (data: { message: string }) => {
       setStatus(data.message);
     });
 
-    socket.on('controllerPairSuccess', (data: { message: string }) => {
-      setStatus('Controller Connected');
+    socket.on("controllerPairSuccess", (data: { playerNumber: number }) => {
+      setPlayerNumber(data.playerNumber);
+      setStatus("Controller Connected");
     });
 
-    socket.on('restartGame', () => {
+    socket.on("restartGame", () => {
       setShowRestartButton(false);
     });
 
-    socket.on('gameOver', () => {
+    socket.on("gameOver", () => {
       setShowRestartButton(true);
     });
 
-    document.addEventListener('click', function userTapPermissions() {
-      document.removeEventListener('click', userTapPermissions, false);
-      // This prevents the phone from going to sleep
-      noSleep.enable();
-    }, false);
-    
-    try {
-      if (DeviceOrientationEvent) {
-        console.log('Device orientation supported!');
-      }
-    } catch(e) {
-      if (parser.getBrowserName().toLowerCase() === 'safari') {
-        setStatus('Your browser does not support motion detection. To enable it on an iOS device, open Settings > Safari > Enable Motion & Orientation Access')      
-      }
-    }
+    document.addEventListener(
+      "click",
+      function userTapPermissions() {
+        document.removeEventListener("click", userTapPermissions, false);
+        // This prevents the phone from going to sleep
+        noSleep.enable();
+      },
+      false
+    );
   }, []);
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    socket.emit('setAssociatedClientId', clientId);
+    socket.emit("setAssociatedClientId", clientId);
   }
 
   function handleChangeClientId(e: React.ChangeEvent<HTMLInputElement>) {
@@ -106,27 +131,68 @@ const App: React.FC = () => {
 
   function handleRestartClick() {
     setShowRestartButton(false);
-    socket.emit('restartGame');
+    socket.emit("restartGame");
   }
 
   function renderRestartButton() {
     if (showRestartButton) {
-      return <div><button onClick={handleRestartClick}>Restart Game</button></div>;
+      return (
+        <div>
+          <button className="restart-button" onClick={handleRestartClick}>
+            <RestartIcon />
+            <p>Restart Game</p>
+          </button>
+        </div>
+      );
     }
     return null;
   }
 
+  async function initializeDeviceOrientationHandler() {
+    if (typeof window.DeviceMotionEvent.requestPermission === "function") {
+      try {
+        const response = await window.DeviceMotionEvent.requestPermission();
+        if (response === "granted") {
+          window.addEventListener("deviceorientation", handleOrientation);
+        } else {
+          console.log("DeviceMotion permissions not granted.");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      window.addEventListener("deviceorientation", handleOrientation);
+    }
+  }
+
+  function handleClickSubmit() {
+    if (!deviceOrientationInitialized) {
+      initializeDeviceOrientationHandler();
+      setDeviceOrientationInitialized(true);
+    }
+  }
+
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input name="clientId" onChange={handleChangeClientId} value={clientId}/>
-        <input type="submit" value="Set Player Id" />
+      <form className="client-id-form" onSubmit={handleSubmit}>
+        <input
+          className="client-id-input"
+          name="clientId"
+          onChange={handleChangeClientId}
+          value={clientId}
+        />
+        <input
+          className="client-submit-button"
+          onClick={handleClickSubmit}
+          type="submit"
+          value="Set Client Id"
+        />
       </form>
-      { status }
-      { !isLandscape && <div>Please turn your device!</div>}
+      {status}
+      {!isLandscape && <div>Please turn your device!</div>}
       {renderRestartButton()}
     </div>
   );
-}
+};
 
 export default App;
